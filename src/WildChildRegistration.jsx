123812ba54fc38d492e9c1830aadcd74e25007dd
Xan <xan.orchid@gmail.com>
2026-04-22 21:20:23 -0600

    remove header padding around logo

--- src/WildChildRegistration.jsx
+++ src/WildChildRegistration.jsx
@@ -208,9 +208,9 @@ export default function WildChildRegistration() {
     <div style={{ fontFamily:"Georgia,serif", background:CREAM, minHeight:"100vh", color:TEXT_DARK }}>
 
       {/* Header */}
-      <div style={{ background:TEAL, padding:"28px 20px 20px", display:"flex", flexDirection:"column", alignItems:"center", gap:"8px" }}>
-        <img src={logo} alt="Wild Child Nosara" style={{ height:"280px", objectFit:"contain", display:"block" }} />
-        <p style={{ fontSize:"11px", letterSpacing:"2px", color:"rgba(255,255,255,0.7)", textTransform:"uppercase", margin:0 }}>Enrollment Registration</p>
+      <div style={{ background:TEAL, padding:"0", display:"flex", flexDirection:"column", alignItems:"center", gap:"0" }}>
+        <img src={logo} alt="Wild Child Nosara" style={{ height:"110px", objectFit:"contain", display:"block" }} />
+        <p style={{ fontSize:"11px", letterSpacing:"2px", color:"rgba(255,255,255,0.7)", textTransform:"uppercase", margin:"0 0 12px" }}>Enrollment Registration</p>
       </div>
 
       {/* Step bar */}
